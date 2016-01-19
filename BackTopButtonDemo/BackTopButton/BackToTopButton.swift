9f1c9a4427ebe73f22f08d0c72515e336d79233b
Neo <RookieNeo1224@163.com>
2016-01-19 20:03:38 +0800

    init project

---
+++ BackTopButtonDemo/BackTopButton/BackToTopButton.swift
@@ -0,0 +1,60 @@
+//
+//  BackToTopButton.swift
+//  BackTopButtonDemo
+//
+//  Created by Neo on 16/1/18.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class BackToTopButton: UIButton {
+    
+    var myContext=0
+    private weak var scrollView:UIScrollView?
+    
+    static func addBackButton(vc:UIViewController,scrollView:UIScrollView, offsetY:CGFloat = 0){
+        let topBtn = BackToTopButton()
+        topBtn.scrollView = scrollView
+        let view=vc.view
+        let width=view.frame.size.width
+        let height=view.frame.size.height
+        let frame = CGRectMake( width-60, height-80-64 + offsetY, 50, 50)
+        topBtn.frame = frame
+        topBtn.setImage(UIImage.init(named: "backBtn"), forState: UIControlState.Normal)
+        topBtn.addTarget(topBtn, action: "topBtnClick:", forControlEvents: UIControlEvents.TouchUpInside)
+        scrollView.addObserver(topBtn, forKeyPath: "contentOffset", options: NSKeyValueObservingOptions.New, context: &topBtn.myContext)
+        view.insertSubview(topBtn, atIndex: view.subviews.count + 1)
+    }
+    
+    func topBtnClick(btn:UIButton){
+        if let tb = self.scrollView as? UITableView{
+            let indexPath=NSIndexPath(forRow: 0, inSection: 0)
+            tb.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
+        }else{
+            UIView.animateWithDuration(0.3) { () -> Void in
+                self.scrollView?.contentOffset=CGPointMake(0, 0)
+            }
+            
+        }
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if context == &myContext {
+            if keyPath=="contentOffset"{
+                if let offset = (change?[NSKeyValueChangeNewKey] as? NSValue)?.CGPointValue(){
+                    if offset.y > 200 {
+                        self.hidden=false
+                    }else{
+                        self.hidden=true
+                    }
+                }
+            }
+        }
+    }
+    
+    deinit{
+        scrollView?.removeObserver(self, forKeyPath: "contentOffset")
+    }
+
+}
---
+++ BackTopButtonDemo/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BackTopButtonDemo
+//
+//  Created by Neo on 16/1/18.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var tableView : UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView = UITableView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width, UIScreen.mainScreen().bounds.size.height), style: UITableViewStyle.Plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+        //You must makesure that the button in the top view
+        BackToTopButton.addBackButton(self, scrollView: tableView)
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var  cell = tableView.dequeueReusableCellWithIdentifier("Rookie_Neo")
+        if (cell == nil)
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Rookie_Neo")
+        }
+        cell!.textLabel!.text = "一个小数据"
+        cell?.backgroundColor = UIColor.orangeColor()
+        return cell!
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
